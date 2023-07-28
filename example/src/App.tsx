2e720508141b1c0f9f4e135e7a56caf094102627
Jhon Herrera <estebanhz9919@gmail.com>
2023-07-28 18:15:26 -0500

    feat: version updated

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,85 +1,11 @@
 import React from 'react'
 
-import {
-  HomeIcon,
-  PostIcon,
-  UserIcon,
-  BanIcon,
-  CarnetIcon,
-  EventIcon,
-  ResumeIcon,
-  JobIcon,
-  NewsIcon,
-  DocIcon,
-  StatisticsIcon,
-  SurveyIcon,
-  TopicIcon,
-  ChatIcon,
-  ForumsIcon,
-  MessageIcon,
-  SecurityIcon,
-  DamagesIcon,
-  ConfigIcon
-} from 'scrinio-react-icons'
+import { ArrowRightIcon } from 'scrinio-react-icons'
 
 const App = () => {
   return (
     <>
-      <HomeIcon />
-      <br />
-      <br />
-      <PostIcon />
-      <br />
-      <br />
-      <UserIcon />
-      <br />
-      <br />
-      <BanIcon />
-      <br />
-      <br />
-      <CarnetIcon />
-      <br />
-      <br />
-      <EventIcon />
-      <br />
-      <br />
-      <ResumeIcon />
-      <br />
-      <br />
-      <JobIcon />
-      <br />
-      <br />
-      <NewsIcon />
-      <br />
-      <br />
-      <DocIcon />
-      <br />
-      <br />
-      <StatisticsIcon />
-      <br />
-      <br />
-      <SurveyIcon />
-      <br />
-      <br />
-      <TopicIcon />
-      <br />
-      <br />
-      <ChatIcon />
-      <br />
-      <br />
-      <ForumsIcon />
-      <br />
-      <br />
-      <MessageIcon />
-      <br />
-      <br />
-      <SecurityIcon />
-      <br />
-      <br />
-      <DamagesIcon />
-      <br />
-      <br />
-      <ConfigIcon />
+      <ArrowRightIcon />
     </>
   )
 }
---
+++ src/arrows/index.ts
@@ -0,0 +1,6 @@
+import ArrowRightIcon from './icons/ArrowRightIcon'
+import ArrowLeftIcon from './icons/ArrowLeftIcon'
+import ArrowUpIcon from './icons/ArrowUpIcon'
+import ArrowDownIcon from './icons/ArrowDownIcon'
+
+export { ArrowRightIcon, ArrowLeftIcon, ArrowUpIcon, ArrowDownIcon }
